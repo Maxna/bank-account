431d35bdb91c4bf7994be42cc578893879934c9f
Garnett Puett and Matt Nardoni <garnett@>
2018-04-03 15:04:21 -0700

    Create basic html and JS layout.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// backend logic
+function Register(initial, subsequent, withdrawal) {
+  this.initial = initialDeposit;
+  this.subsequent = deposit;
+  this.withdrawal = withdraw;
+}
+
+Register.prototype.balance = function() {
+  var balance = "";
+
+}
+
+
+// User interface
+$(function() {
+  newBankAccount = new bankAccount ();
+});
